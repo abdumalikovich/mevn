b931b94c71f761211e8adf24f8c482ce9223380e
Shakh Khamidov <38461365+abdumalikovich@users.noreply.github.com>
2020-05-13 22:57:58 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,21 @@ import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import router from './router'
 import store from './store'
-import HighchartsVue from 'highcharts-vue'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import VueMeta from 'vue-meta'
 
 Vue.use(Vuex)
 Vue.use(VueRouter)
-Vue.use(HighchartsVue)
+Vue.use(VueMeta)
+// Vue.use(HighchartsVue)
 
 Vue.config.productionTip = false
 
 new Vue({
+	created() {
+		AOS.init()
+	},
 	router,
 	store,
 	render: h => h(App),
