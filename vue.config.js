3e90e6669ec2fe1fd6c52ac9e2803e1877a9801c
Shakh Khamidov <38461365+abdumalikovich@users.noreply.github.com>
2020-05-13 22:27:09 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,14 @@ module.exports = {
 	publicPath: '/',
 	outputDir: path.resolve(__dirname, '../server/public'),
 	devServer: {
-		proxy: 'http://localhost:80'
+		proxy: 'http://localhost:3000'
 	},
 	pluginOptions: {
 		layouts: {
-			preloader: false
 		},
 	}
 }
 
 if(process.env.NODE_ENV === 'production') {
-	module.exports.devServer.proxy = 'http://wepro.uz';
-	module.exports.pluginOptions.layouts.preloader = true;
+	module.exports.devServer.proxy = 'http://example.com';
 }
